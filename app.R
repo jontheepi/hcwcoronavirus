51e10ad64ccceff499c636324a8449e53e6499e0
Jonathan Gross <jonathan.gross@baltimorecity.gov>
2020-04-16 21:52:21 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -29,7 +29,7 @@ ui <- fluidPage(
                
    navbarPage("Healthcare worker deaths from novel Coronavirus (COVID-19) in the US",
           
-        tabPanel("Map",h4("70 reported deaths as of 4/15/2020 8:30 PM", style = "color:#ff0000"),
+        tabPanel("Map",h4("78 reported deaths as of 4/16/2020 9:50 PM", style = "color:#ff0000"),
                  h4("This app visualizes US healthcare worker deaths from coronavirus (COVID-19) that are reported in the news. It is an underestimate."),
                  h4("Mapped location is work/facility location. Unknown locations are placed at state capitol or nearest city. In some cases, the source of transmission (work vs. community) may be unclear."),
                  leafletOutput("map",width="100%",height="500px"),
@@ -43,9 +43,10 @@ ui <- fluidPage(
         tabPanel("Attribute Table",dataTableOutput("table"),style = "font-size:90%"),
         tabPanel("Time series",plotOutput("time")),
         #tabPanel("Age", plotOutput("age")),
-        tabPanel("By Worker Type",plotOutput("type")),
+        tabPanel("By Worker Type",plotOutput("type",height="600px")),
         tabPanel("By Age",plotOutput("age")),
         tabPanel("By Sex",plotOutput("sex")),
+        tabPanel("By Facility Type",plotOutput("facility",height="600px")),
         tabPanel("Summary Statistics",uiOutput("stats"))
         #"tabPanel("State")
             )
@@ -84,7 +85,7 @@ server <- function(input, output) {
      timedata<-mapdata
      timedata$Report_Date<-as.Date(timedata$Report_Date,"%m/%d/%Y")
      timedata$cnt<-1
-     timedata<-timedata[,c(1,22)]
+     timedata<-timedata[,c(1,23)]
      time_agg<-aggregate(timedata$cnt,by=list(timedata$Report_Date),sum)
      temporal<-ggplot(time_agg, aes(x = Group.1, y = x))+  geom_bar(stat="identity")+scale_x_date(date_labels = "%m/%d/%Y", date_breaks = "1 day") +
        scale_y_discrete(limits=c(0,1,2,3,4,5,6,7,8,9,10))+theme(text = element_text(size=18),axis.text.x = element_text(angle = 90))
@@ -96,11 +97,11 @@ server <- function(input, output) {
      output$type<-renderPlot({
        aged<-mapdata
        aged$cnt<-1
-       aged<-aged[,c(7,22)]
+       aged<-aged[,c(7,23)]
        aged2<-aggregate(x=aged$cnt,FUN=sum,by=list(aged$Type))
        aged2<- aged2[order(-aged2$x),]
        ageplot<-ggplot(data=aged2,aes(x=reorder(Group.1,-x),y=x))+geom_bar(stat="identity")
-       ageplot+xlab("Type")+ylab("Number of Deaths")+ggtitle("Deaths from COVID-19, by Healthcare Worker Type")+theme(text = element_text(size=18),axis.text.x = element_text(angle = 90))+ ylim(0,30)+
+       ageplot+xlab("Type")+ylab("Number of Deaths")+ggtitle("Deaths from COVID-19, by Healthcare Worker Type")+theme(text = element_text(size=18))+theme(axis.text.x = element_text(angle = 45,hjust=1))+ ylim(0,30)+
          geom_text(aes(label=x),vjust=-1,size=6)
      })
      
@@ -111,7 +112,7 @@ server <- function(input, output) {
      output$age<-renderPlot({
        qplot(age_data$Age, geom="histogram",binwidth=5,fill=age_data$Type)+xlab("Age")+
          ylab("Number of Deaths")+ggtitle("Histogram of HCW Deaths from COVID-19, by Age and Type")+
-         theme(text = element_text(size=20))+ylim(0,15)+scale_fill_discrete(name = "Type of HCW")
+         theme(text = element_text(size=20))+ylim(0,15)+scale_fill_discrete(name = "Type of HCW")+scale_x_continuous(breaks=c(20,30,40,50,60,70,80))
          
      })
      
@@ -127,9 +128,21 @@ server <- function(input, output) {
          geom_text(aes(label=x),vjust=-1,size=6)
      })
      
+     # Facility Statistics
+     output$facility<-renderPlot({
+       fac<-mapdata
+       fac$cnt<-1
+       fac_agg<-aggregate(fac$cnt,by=list(fac$Facility_Sub),FUN=sum)
+       fac_agg$Group.1=as.character(fac_agg$Group.1)
+       fac_agg<- fac_agg[order(-fac_agg$x),]
+       #sex_agg$Group.1[sex_agg$Group.1==""]<-"Missing"
+       ggplot(fac_agg,aes(x=reorder(Group.1,-x),y=x))+geom_bar(stat="identity")+
+        xlab("Facility Type")+ylab("Number of Deaths")+ggtitle("HCW Deaths from COVID-19, by Facility Type")+theme(text = element_text(size=20))+theme(axis.text.x = element_text(angle = 45,hjust=1))+ylim(0,50)+scale_fill_discrete(name = "Facility Type")+geom_text(aes(label=x),vjust=-1,size=6)
+     })
+     
 
      # Summary statistics
-     allvariables<- mapdata %>% dplyr::select(Age,Sex,Type,Facility, City,State,Zipcode)
+     allvariables<- mapdata %>% dplyr::select(Age,Sex,Type,Facility, Facility_Sub,City,State,Zipcode)
      overview<-dfSummary(allvariables,max.distinct.values = 50)
      output$stats<-renderUI({
        view(overview,
